8d7d167bccb5bf38fc0fdf9344893498b24a8715
xd-vape <mehdi33889@gmail.com>
2023-05-24 20:16:53 +0200

    few things change

--- src/Events/Client/ready.js
+++ src/Events/Client/ready.js
@@ -1,16 +1,30 @@
+require("dotenv").config();
 const { version } = require("discord.js");
 const { loadCommands } = require("../../Handlers/commandHandler");
 const ConsoleLogger = require("../../Structures/Classes/consoleLogger");
 const logger = new ConsoleLogger();
 
+const mongoose = require("mongoose");
+
 module.exports = {
   name: "ready",
   once: true,
-  execute(client) {
+  async execute(client) {
     loadCommands(client);
+
+    connectMongoDB();
 
     setTimeout(async () => {
       logger.success(`Client • ready to use (v.${version})\n---`);
     }, 1000);
   },
 };
+
+async function connectMongoDB() {
+  try {
+    await mongoose.connect(process.env.DATABASEURL);
+    logger.success("Mongoose Database • connected");
+  } catch (err) {
+    logger.error(err);
+  }
+}
